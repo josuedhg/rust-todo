995a478a7f658f92cfaa605799c4f8805dbc03c0
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2025-12-03 20:18:50 -0600

    refactor(main): replace unwraps with matches
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use todo::Task;
 use std::fs::OpenOptions;
-use todo::TodoTxt;
+use todo::Task;
 use todo::Todo;
+use todo::TodoTxt;
 
 fn main() {
-    let file = OpenOptions::new().read(true).write(true).create(true).open("todo.txt").unwrap();
+    let file = match OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("todo.txt")
+    {
+        Ok(file) => file,
+        Err(e) => panic!("Could not open file: {}", e),
+    };
     let mut todo = TodoTxt::new(Box::new(file));
     let task = Task::new(String::from("Learn Rust"), String::from("Learn"), 'A');
-    todo.add(task).unwrap();
-    for task in todo.list().unwrap() {
+
+    match todo.add(task) {
+        Ok(_) => (),
+        Err(e) => panic!("Could not add task: {}", e),
+    };
+
+    let list = match todo.list() {
+        Ok(list) => list,
+        Err(e) => panic!("Could not list tasks: {}", e),
+    };
+    for task in list {
         println!("{}", task.name);
     }
 }
