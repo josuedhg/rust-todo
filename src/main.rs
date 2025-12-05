cf3ff0d2dd44eebe7a49cb44f4bbb7b2565e930e
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2025-12-04 19:14:12 -0600

    feature(main): add command line arguments (add, list)
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
+use clap::{Parser, Subcommand};
 use std::fs::OpenOptions;
 use todo::Task;
 use todo::Todo;
 use todo::TodoTxt;
 
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Add { task: Vec<String> },
+    List,
+}
+
+impl Command {
+    fn run(&self, todo: &mut TodoTxt) {
+        match self {
+            Command::List => {
+                let list = match todo.list() {
+                    Ok(list) => list,
+                    Err(e) => panic!("Could not list tasks: {}", e),
+                };
+                for task in list {
+                    println!("{}", task.name);
+                }
+            }
+            Command::Add { task } => {
+                let newtask = Task::new(task.join(" "), String::from(""), 'A');
+
+                match todo.add(newtask) {
+                    Ok(_) => (),
+                    Err(e) => panic!("Could not add task: {}", e),
+                };
+            }
+        }
+    }
+}
+
 fn main() {
+    let args = Cli::parse();
+
     let file = match OpenOptions::new()
         .read(true)
         .write(true)
@@ -14,18 +53,9 @@ fn main() {
         Err(e) => panic!("Could not open file: {}", e),
     };
     let mut todo = TodoTxt::new(Box::new(file));
-    let task = Task::new(String::from("Learn Rust"), String::from("Learn"), 'A');
 
-    match todo.add(task) {
-        Ok(_) => (),
-        Err(e) => panic!("Could not add task: {}", e),
+    match args.command {
+        Some(command) => command.run(&mut todo),
+        None => println!("No command provided"),
     };
-
-    let list = match todo.list() {
-        Ok(list) => list,
-        Err(e) => panic!("Could not list tasks: {}", e),
-    };
-    for task in list {
-        println!("{}", task.name);
-    }
 }
